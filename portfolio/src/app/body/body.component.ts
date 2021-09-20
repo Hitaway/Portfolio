ac3be42e08750e26579cfa74412ea6779c4e611a
Hitaway <quentinrat@hotmail.fr>
2021-09-20 23:51:04 +0200

    Updating the list of words displayed on the body

--- portfolio/src/app/body/body.component.ts
+++ portfolio/src/app/body/body.component.ts
@@ -8,7 +8,7 @@ import { OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
-  public listWorld: string[] = ['Développeur', 'Blockchain Enthousiaste'];
+  public listWorld: string[] = ['Développeur', 'Front end','Blockchain Enthousiaste'];
   public worldToWrite: string = '';
 
   ngOnInit() {
